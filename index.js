35f2a3b3c80dc4bffa66406fafb652b7cd311c0b
ngtambt94@gmail.com <ngochan94>
2017-10-06 16:00:47 +0700

    update menu

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/webhook', function (req, res) {
     let sender = event.sender.id
     if (event.message && event.message.text) {
       let text = event.message.text
-      if (text === 'Generic' || text === 'generic' || text === 'help' || text === 'Help'){ 
+      if (text === 'Generic' || text === 'generic' || text === 'help' || text === 'Help' || text === 'menu' || text === 'Menu'){ 
         // console.log("welcome to chatbot")
         sendGenericMessage(sender)
         // continue
