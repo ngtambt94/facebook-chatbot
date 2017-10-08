a9eb13bfbd1a4b02af39364723f49eab3277ca55
ngtambt94@gmail.com <ngochan94>
2017-10-08 20:51:03 +0700

    commit

--- index.js
+++ index.js
@@ -8,21 +8,13 @@ const app = express()
 // aiml
 AIMLInterpreter = require('./AIMLInterpreter');
 
-var aimlInterpreter = new AIMLInterpreter({name:'Tam Nguyen', age:'23'});
-aimlInterpreter.loadAIMLFilesIntoArray(['./template.xml']);
+var aimlInterpreter = new AIMLInterpreter({name:'WireInterpreter', age:'42'});
+aimlInterpreter.loadAIMLFilesIntoArray(['./templatexml']);
 
 var callback = function(answer, wildCardArray, input){
     console.log(answer + ' | ' + wildCardArray + ' | ' + input);
 };
 
-var caseCallback = function(answer, wildCardArray, input){
-  if (answer == this) {
-    console.log(answer + ' | ' + wildCardArray + ' | ' + input);
-  } else {
-    console.log('ERROR:', answer);
-    console.log('   Expected:', this.toString());
-  }
-};
 
 app.set('port', (process.env.PORT || 5000))
 
